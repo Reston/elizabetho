60fb8063d59766eece22e4d73852d1e006c4eaeb
JOSE ORDOÑEZ <restonjd@gmail.com>
2015-07-14 16:08:22 -0430

    some fixes

--- elizabetho/elizabetho/apps/homepage/models.py
+++ elizabetho/elizabetho/apps/homepage/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Testimonial(models.Model):
 	name = models.CharField(max_length=100)
-	description = models.CharField(max_length=240)
+	description = models.CharField(max_length=600)
 	photo = models.ImageField(upload_to='imgtestimonials')
 	create_in = models.DateTimeField(auto_now_add=True, editable=False)
 	modify_on = models.DateTimeField(auto_now=True)
